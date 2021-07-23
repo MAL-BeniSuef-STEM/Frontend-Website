f23b2995dfc8ff4fc3d15a7455fb8aa52ff63f10
ahmed992 <ahmedalkpeer.v2@gmail.com>
2021-07-24 01:33:08 +0200

    error page

---
+++ pages/404.js
@@ -0,0 +1,37 @@
+import { useTranslations } from "next-intl";
+
+// Components
+import Layout from "../layout";
+import { SEO } from "../components";
+import NavLink from "../components/Header/NavLink";
+
+function Error() {
+	const t = useTranslations();
+	return (
+		<Layout>
+			<SEO title="About US" />
+			<div className="h-screen text-center bg-no-repeat bg-cover bg-hero-pattern bg-brand-gray">
+				<h1 className="pt-56 font-bold leading-tight tracking-wider ">
+					{t("404")}
+				</h1>
+				<h2>{t("t-2")}</h2>
+				<p className="mt-6 text-[#797979]">
+					{t("t-3")} <NavLink href="/" text={t("t-home")} />{" "}
+				</p>
+			</div>
+		</Layout>
+	);
+}
+
+export function getStaticProps({ locale }) {
+	return {
+		props: {
+			messages: {
+				...require(`../messages/shared/${locale}.json`),
+				...require(`../messages/error/${locale}.json`)
+			}
+		}
+	};
+}
+
+export default Error;
